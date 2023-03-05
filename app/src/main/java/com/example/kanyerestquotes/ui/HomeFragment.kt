c210e24f9b920ba07d34e65701ec3dd2fb85836b
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-03-05 16:28:05 +0100

    feat : announcer aktiviert (Homefragment)

--- app/src/main/java/com/example/kanyerestquotes/ui/HomeFragment.kt
+++ app/src/main/java/com/example/kanyerestquotes/ui/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.kanyerestquotes.ui
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +11,14 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.kanyerestquotes.MainViewModel
 import com.example.kanyerestquotes.databinding.HomeFragmentBinding
+import java.util.*
 
-class HomeFragment: Fragment() {
+
+class HomeFragment: Fragment(), TextToSpeech.OnInitListener {
 
     private lateinit var binding: HomeFragmentBinding
+
+    private lateinit var tts: TextToSpeech
 
     private val viewModel: MainViewModel by activityViewModels()
 
@@ -20,6 +26,7 @@ class HomeFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
 
     ): View? {
 
@@ -30,6 +37,11 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        tts = TextToSpeech(requireContext(), this)
+        tts.setLanguage(Locale.ROOT)
+        tts.setPitch(0.2f)
+        tts.setSpeechRate(1.0f)
+
         binding.refreshButton.setOnClickListener {
             viewModel.getQuote()
             viewModel.buttonAnimator(binding.refreshButton)
@@ -38,9 +50,30 @@ class HomeFragment: Fragment() {
         viewModel.quote.observe(viewLifecycleOwner, Observer {
             if (it != null) {
                 binding.quotesTextHome.setText(it.quote)
+                speakOut(it.quote)
             }
         })
 
-
     }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.language = Locale.getDefault()
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+
+    private fun speakOut(text: String) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+        if (tts.isSpeaking) {
+            tts.stop()
+        }
+        tts.shutdown()
+        super.onDestroy()
+    }
+
 }
